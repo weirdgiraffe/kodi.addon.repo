60336738b6d7e0b0fa9894c780a7594a7833a733
weirdgiraffe <giraffe@cyberzoo.xyz>
2016-12-11 14:17:13 +0100

    add script to update repository

---
+++ updaterepo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+#
+# update.sh
+# Copyright (C) 2016 weirdgiraffe <giraffe@cyberzoo.xyz>
+#
+# Distributed under terms of the MIT license.
+#
+
+git clone --depth=1 git@github.com:weirdgiraffe/plugin.video.giraffe.seasonvar.git tmp/plugin.video.giraffe.seasonvar
+cd tmp/plugin.video.giraffe.seasonvar
+git fetch --tags
+latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+echo checking out ${latestTag}
+cp addon.xml ../../addons/plugin.video.giraffe.seasonvar/
+cp changelog.txt ../../addons/plugin.video.giraffe.seasonvar/
+cp fanart.jpg ../../addons/plugin.video.giraffe.seasonvar/
+cp icon.png ../../addons/plugin.video.giraffe.seasonvar/
+git archive \
+  --format=zip \
+  --prefix=plugin.video.giraffe.seasonvar/ \
+  --output ../../addons/plugin.video.giraffe.seasonvar/plugin.video.giraffe.seasonvar-${latestTag}.zip \
+  HEAD
+cd ../../addons/
+python genaddons.py
+echo "done"
+
+
+
